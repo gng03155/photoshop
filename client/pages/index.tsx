a01705e1f994614fd20bbbd434ad17f2b9470f3d
jongseon <gng03155@gmail.com>
2021-06-01 18:02:13 +0900

    Next.js + Typescript 생성

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+export default function Home() {
+  const [text, setText] = useState<string>("자바스크립트");
+  return (
+    <div className="container">
+      <div>
+        <span>{text} 적용 완료</span>
+      </div>
+    </div>
+  );
+}
