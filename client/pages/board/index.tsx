a595ee25df7c55ba3a1cb99edabc44623b18f8ce
jongseon <gng03155@gmail.com>
2021-06-18 00:17:49 +0900

    wiziwig 에디터 선정완료...

--- client/pages/board/index.tsx
+++ client/pages/board/index.tsx
@@ -1,34 +1,59 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { CKEditor } from 'ckeditor4-react';
+// import dynamic from 'next/dynamic'
+// const Editor = dynamic(() => import('../../src/components/test'), {
+//   ssr: false
+// })
+// import {} from "../../src/ckeditor/ckeditor"
+
 
 export default function Board() {
-    const editorRef = useRef()
-    const [ editorLoaded, setEditorLoaded ] = useState( false )
-    const { CKEditor, ClassicEditor} = editorRef.current || {}
+  const [data, setData] = useState("");
+  useEffect(() => {
+    console.log(data);
+  }, [data])
 
-    useEffect( () => {
-        editorRef.current = {
-          CKEditor: require( '@ckeditor/ckeditor5-react' ).CKEditor, //Added .CKEditor
-          ClassicEditor: require( '@ckeditor/ckeditor5-build-classic' ),
-        }
-        setEditorLoaded( true )
-    }, [] );
-    
-   const [data, setData] = useState('');
 
-    return( 
-      <>
-        {editorLoaded ? <CKEditor
-            editor={ ClassicEditor }
-            data={data}
-            onReady={ editor => {
-              // You can store the "editor" and use when it is needed.
-              console.log('Editor is ready to use!', editor);           
-            } }
-            onChange={ (event, editor ) => {
-              const data = editor.getData()
-              setData(data);
-            } }
-        /> : <p>Carregando...</p>}
-     </>
-     )
+  return (
+    <div>
+      <CKEditor
+        editorUrl='//cdn.ckeditor.com/4.16.1/full/ckeditor.js'
+        onChange={(evt) => { setData(evt.editor.getData()) }}
+        config={{
+          toolbar: [
+            { name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
+            { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language'] },
+            { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat'] },
+            { name: 'colors', items: ['TextColor', 'BGColor'] },
+            { name: 'clipboard', items: ['Undo', 'Redo'] },
+            { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+            '/',
+            '/',
+          ],
+          // toolbarGroups: [
+          //   { name: 'styles', groups: ['styles'] },
+          //   { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+          //   '/',
+          //   { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+          //   { name: 'colors', groups: ['colors'] },
+          //   { name: 'document', groups: ['mode', 'doctools', 'document'] },
+          //   { name: 'clipboard', groups: ['clipboard', 'undo'] },
+          //   { name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+          //   { name: 'forms', groups: ['forms'] },
+          //   { name: 'links', groups: ['links'] },
+          //   '/',
+          //   { name: 'insert', groups: ['insert'] },
+          //   '/',
+          //   { name: 'tools', groups: ['tools'] },
+          //   { name: 'others', groups: ['others'] },
+          //   { name: 'about', groups: ['about'] }
+          // ],
+          removeButtons:
+            'Find,SelectAll,Scayt,Replace,Cut,Copy,Paste,PasteText,PasteFromWord,Templates,Save,Source,NewPage,ExportPdf,Preview,Print,TextField,Textarea,Select,Button,ImageButton,HiddenField,Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Maximize,About,ShowBlocks,Form,Checkbox,Radio',
+          width: 600,
+          height: 400,
+        }}
+      />
+    </div>
+  )
 }
---
+++ client/typings.d.ts
@@ -0,0 +1,27 @@
+// declare module '@ckeditor/*' {
+//     const classes: any;
+//     export default classes;
+// }
+
+// declare module "@ckeditor/ckeditor5-react" {
+//     const CKEditor: any;
+//     export default CKEditor;
+// }
+
+// declare module "@ckeditor/ckeditor5-build-classic" {
+//     const ClassicEditor: any;
+//     export = ClassicEditor;
+// }
+// declare module "@ckeditor/ckeditor5-react" {
+// }
+
+// declare module "@ckeditor/ckeditor5-build-classic" {
+//     export const ClassicEditor: any;
+// }
+// declare module "'@ckeditor/ckeditor5-editor-classic/src/classiceditor" {
+//     export const ClassicEditor: any;
+// }
+// declare module "ckeditor4-react" {
+//     const CKEditor: any;
+//     export = CKEditor;
+// }
