0726f3622921998fc225d997c697c786bd9ca650
jongseon <gng03155@gmail.com>
2021-07-28 01:05:10 +0900

    오류수정

--- client/src/components/MypageComponents/UserBoardManager/index.tsx
+++ client/src/components/MypageComponents/UserBoardManager/index.tsx
@@ -15,7 +15,7 @@ export default function UserBoardManager({ userKey }: Props) {
 
     const router = useRouter();
 
-    const { data: userBoardList } = useSWR<string[] | undefined>(`board/user/${userKey}`, fetcherData, { revalidateOnMount: true, initialData: null });
+    const { data: userBoardList, revalidate } = useSWR<string[] | undefined>(`board/user/${userKey}`, fetcherData, { revalidateOnMount: true, initialData: null });
 
 
     if (userBoardList === null) {
@@ -25,7 +25,7 @@ export default function UserBoardManager({ userKey }: Props) {
     return (
         <Wrap>
             <h2>게시물관리</h2>
-            <BoardList userKey={userKey} boardKeyList={userBoardList !== undefined ? Object.keys(userBoardList) : []} category="user" />
+            <BoardList userKey={userKey} boardKeyList={userBoardList !== undefined ? Object.keys(userBoardList) : []} category="user" boardRevalidate={revalidate} />
         </Wrap>
     )
 }
